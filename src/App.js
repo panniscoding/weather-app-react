ee57156eb2d63dc45947b54d124be74a730dac69
pansypanneiphyu <pansy.miss@gmail.com>
2025-12-03 13:04:23 +0100

    Added Bootstrap
    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Pann and is
-        <a
-          href="https://github.com/panniscoding/weather-app-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          open-sourced on Github{" "}
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Pann and is
+          <a
+            href="https://github.com/panniscoding/weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced on Github{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
