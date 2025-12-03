32c100466d4da1be1c57bdd1c6180194949e42ce
pansypanneiphyu <pansy.miss@gmail.com>
2025-12-03 14:09:56 +0100

    Added basic HTML
    Added basic HTML

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,43 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello from weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1> New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
+            alt="Mostly Cloudy"
+          />
+          8°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 19%</li>
+            <li>Humidity: 83%</li>
+            <li>Wind: 10km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
