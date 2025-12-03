dc4916bc1d4b8de3d69132ce4ee9966ff3f70fcc
pansypanneiphyu <pansy.miss@gmail.com>
2025-12-03 13:20:40 +0100

    Added Weather.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Pann and is
           <a
