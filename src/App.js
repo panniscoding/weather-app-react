eeb72ac505e28de69c742e9c88f40323ae2c7fcb
pansypanneiphyu <pansy.miss@gmail.com>
2025-12-03 12:44:15 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Pann and is
+        <a
+          href="https://github.com/panniscoding/weather-app-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          open-sourced on Github{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
