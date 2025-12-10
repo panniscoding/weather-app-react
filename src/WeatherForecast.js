b682f1b1325cf245c21263fa806667f9febb78e4
pansypanneiphyu <pansy.miss@gmail.com>
2025-12-10 10:44:58 +0100

    Added Forecast Layout&API
    Added basic Forecast Layout and API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecast(props) {
+  let apiKey = "17t3ob19948f5f5bf884a243a9005805";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast-day">
       <div className="row">
